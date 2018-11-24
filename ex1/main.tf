1782dadd56669f30140d7741944f6bfc044d481f
David Paul <dfp333@gmail.com>
2018-11-23 19:52:48 -0600

    added outputs and variables to make more DRY

--- ex1/main.tf
+++ ex1/main.tf
@@ -1,6 +1,10 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" {}
+variable "server_port" {
+  description = "posrt the server will use for http requests"
+  default = 8080
+}
 
 provider "aws" {
     access_key = "${var.aws_access_key}"
@@ -16,7 +20,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
   tags {
@@ -28,9 +32,13 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
