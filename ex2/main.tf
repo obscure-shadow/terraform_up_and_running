4f608059ce705eb615d91a200150889c5b245de3
David Paul <dfp333@gmail.com>
2018-11-25 15:57:39 -0600

    rolling back to fix git error

---
+++ ex2/main.tf
@@ -0,0 +1,33 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
+
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
+
+resource "aws_s3_bucket" "terraform_state"{
+  bucket = "terraform-up-and-runnng-state-david"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+/* terraform remote config doesn't seem to be a thing anymore, attempting workaround with thie */
+
+terraform {
+  backend "s3" {
+  bucket="terraform-up-and-runnng-state-david"
+  key="terraform.tfstate"
+  region="us-east-1"
+  }
+}
